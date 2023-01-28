ba88971f4a2bcbe83d5653ecbe604f9359b85bce
Code Sandbox <hello@codesandbox.io>
2023-01-28 17:34:04 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4e4bf2c426ba907778b5db67a69ba9d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2b467268644f281efb01a30ea45db4",
+    "url": "https://danishahmedkhan11.github.io/csb-zgjqhk/index.html"
+  },
+  {
+    "revision": "50e3990e9f427ba3ae35",
+    "url": "https://danishahmedkhan11.github.io/csb-zgjqhk/static/js/2.3b5778c1.chunk.js"
+  },
+  {
+    "revision": "c7a7c12a3f59df51f44b",
+    "url": "https://danishahmedkhan11.github.io/csb-zgjqhk/static/js/main.2ba232aa.chunk.js"
+  },
+  {
+    "revision": "37757196e64e91c5dfff",
+    "url": "https://danishahmedkhan11.github.io/csb-zgjqhk/static/js/runtime-main.d23e43b0.js"
+  }
+]);
